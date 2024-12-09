e75e958efca9a700cfbccde5410b3d91bb297eb4
jakos322 <jakos322@student.liu.se>
2024-12-09 20:39:47 +0100

    yoyo

--- start.sh
+++ start.sh
@@ -20,12 +20,7 @@ else
   echo "Submodules initialized and updated."
 fi
 
-echo "removing previous containers" > /dev/null 2>&1
-docker remove ssl-viewer > /dev/null 2>&1
-docker remove game-controller > /dev/null 2>&1
-docker remove vision > /dev/null 2>&1
-docker remove ai > /dev/null 2>&1
-docker remove viewer > /dev/null 2>&1
+docker compose down
 
 #-----------------------------------------------------------#
 #    Docker configurations                                  #
@@ -38,10 +33,10 @@ echo "2. Local controller"
 echo "3. local game viewer"
 echo "4. Local controller and local game viewer"
 echo "5. Start real SSL vision (only on fetdator)"
-read -p "Enter your choice (1-5): " choice
+read -p "$(echo -e "${GREEN}Enter your choice (1-5):${NC}")" choice
 
 echo -e "${CYAN}closing previous containers${NC}"
-docker stop $(docker ps -q) # close all previous running containers 
+docker compose down
 echo -e "${CYAN}Website: ${RED}http://localhost:5173/${NC}"
 echo -e "${CYAN}Other website: ${RED}http://localhost:8082/${NC}"
 
@@ -50,28 +45,28 @@ echo -e "${CYAN}Other website: ${RED}http://localhost:8082/${NC}"
 case $choice in
   1)
     echo "Starting base configuration..."
-    docker compose -p base-config up -d
+    docker compose -p config1 up -d
     ;;
   2)
     echo "Starting with local controller and GHCR game viewer..."
-    docker compose -f docker-compose.yml -f docker-compose.local-controller.yml -p controller-config up --build -d --force-recreate
+    docker compose -p config2 -f docker-compose.yml -f docker-compose.local-controller.yml up --build -d
     echo -e "${CYAN}Entering controller container...${NC}"
-    docker compose -p controller-config exec controller sh
+    docker compose -p config2 exec controller sh
     ;;
   3)
     echo "Starting with local game viewer and GHCR controller..."
-    docker compose -f docker-compose.yml -f docker-compose.local-gameviewer.yml -p gameviewer-config up --build -d --force-recreate
+    docker compose -p config3 -f docker-compose.yml -f docker-compose.local-gameviewer.yml up --build -d
     echo -e "${CYAN}Entering game-viewer container...${NC}"
-    docker compose -p gameviewer-config exec game-viewer bash
+    docker compose -p config3 exec game-viewer bash
     ;;
   4)
     echo "Starting with local controller and local game viewer..."
-    docker compose -f docker-compose.yml -f docker-compose.local-controller.yml -f docker-compose.local-gameviewer.yml -p dev-config up --build -d --force-recreate
+    docker compose -p config4 -f docker-compose.yml -f docker-compose.local-controller.yml -f docker-compose.local-gameviewer.yml up --build -d --force-recreate
     echo -e "${GREEN}Both controller and game-viewer are local. Not entering any container.${NC}"
     ;;
   5)
     echo "Starting setup for real cameras"
-    docker compose -f docker-compose.yml -f docker-compose.real-robots.yml -f docker-compose.local-controller.yml -p gameviewer-config up --build -d --force-recreate
+    docker compose -p config5 -f docker-compose.yml -f docker-compose.real-robots.yml -f docker-compose.local-controller.yml up --build -d --force-recreate
     echo -e "${GREEN}Everything is started. Not entering any container.${NC}"
     ;;
   *)
